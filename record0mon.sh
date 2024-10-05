d1f9dbbc0d3ee69d13425e83f705346ae53b8128
TrekkaRei <TrekkaRei@gmail.com>
2024-10-05 20:33:26 +0200

    update

--- record0mon.sh
+++ record0mon.sh
@@ -1,2 +1,2 @@
-gnome-terminal --geometry=70x6+10+10 -t "ffmpeg record-0" -x ffmpeg -y -f x11grab -s 2560x1440 -i :0.0 -f pulse -ac 2 -i default /home/trekka/Videos/ffmpeg/rec0.mkv
+gnome-terminal --geometry=70x6+10+10 -t "ffmpeg record-0" -- ffmpeg -y -f x11grab -s 2560x1440 -i :0.0 -f pulse -ac 2 -i default /home/trekka/Videos/ffmpeg/rec0.mkv
 xprop -name 'ffmpeg record-0' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
