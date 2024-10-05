a5924fa3ac355762c4761c0a18f2b6974cbdcfcf
TrekkaRei <TrekkaRei@gmail.com>
2024-10-05 21:28:51 +0200

    update

--- ytd.sh
+++ ytd.sh
@@ -1,2 +1,2 @@
-gnome-terminal --geometry=58x6+2025+10 -t "yt-dl" -- yt-dlp -x --verbose --audio-format "mp3" --audio-quality 0 "$*" -o "~/Music/yt-dl/%(title)s.%(ext)s"
+gnome-terminal --geometry=58x6+2020+15 -t "yt-dl" -- yt-dlp -x --verbose --audio-format "mp3" --audio-quality 0 "$*" -o "~/Music/yt-dl/%(title)s.%(ext)s"
 xprop -name 'yt-dl' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
