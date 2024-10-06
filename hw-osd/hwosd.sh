933b120a72e70ebdf2b45722b3481440b76c82ee
TrekkaRei <TrekkaRei@gmail.com>
2024-10-06 22:41:53 +0200

    updated osd

--- hw-osd/hwosd.sh
+++ hw-osd/hwosd.sh
@@ -2,7 +2,6 @@
 
 cputemp=$(sensors | grep -w "Tctl" | cut -c 16-21 | cut -c 1-2)
 gputemp=$(nvidia-smi | grep -w Default | cut -c 9-12 | cut -c 1-2)
-# cpuusage=$(top -i -bn 1 | grep '%Cpu' | awk '{print $2+$4+$6}')
 cpuusage=$(top -i -bn 1 | grep '%Cpu' | awk '{print 100-$8}')
 gpuusage=$(nvidia-smi -q | grep -w "Gpu" | awk '{print $3}')
 memusage=$(fastfetch --logo none | grep RAM | cut -c 7-11)
