808f676e399d39c2960fdb19666a353615f31d9a
TrekkaRei <TrekkaRei@gmail.com>
2024-10-14 18:34:01 +0200

    update

--- podc.sh
+++ podc.sh
@@ -1,2 +1,2 @@
-gnome-terminal --geometry=50x4+1600+15 -t "sterm" -- mpv --no-video "$*"
+gnome-terminal --geometry=50x4+1020+1440 -t "sterm" -- mpv --no-video "$*"
 xprop -name 'sterm' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
